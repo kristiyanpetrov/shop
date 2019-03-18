3526c038c6a06602170f64d8fabf2946ed930bfe
kristiyanpetrov8 <kristiyan.petrov@umbicord.com>
2019-03-18 10:45:37 +0000

    added a lot of things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shoping-angular-project';
+    loadFeature = 'recipe';
+
+    onNavigate(feature: string) {
+        this.loadFeature = feature;
+    }
 }
